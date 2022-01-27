40810bfa2cd454998106e06b320eee80094edd0b
Almir Garic <almir.garic.1992@gmail.com>
2022-01-27 11:41:43 +0100

    Initial Commit

---
+++ FoodyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodyApp
+//
+//  Created by Macbook on 27. 1. 2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
